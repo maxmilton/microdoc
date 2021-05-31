3dba2187d6e41c74c1fdfb25d439ad8d46a51bc9
Max Milton <max@wearegenki.com>
2021-05-31 19:06:18 +1000

    Fix TS linting

--- xcss.config.js
+++ xcss.config.js
@@ -3,8 +3,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 const { onBeforeBuild } = require('ekscss');
+// @ts-expect-error - no types yet
 const { preloadApply } = require('@ekscss/framework/utils');
 
 onBeforeBuild(preloadApply);
 
+// @ts-expect-error - no types yet
 module.exports = require('@ekscss/framework/config');
