acf994acac9957e49d38b2eed76676bfdcb2cc6d
Max Milton <max@wearegenki.com>
2021-09-26 08:08:05 +1000

    Fix path in test

--- test/index.test.ts
+++ test/index.test.ts
@@ -11,7 +11,7 @@ test.after.each(teardown);
 
 test('renders microdoc core app', () => {
   // eslint-disable-next-line import/extensions, global-require
-  require('../dist/microdoc.js');
+  require('../microdoc.js');
 
   // TODO: Better assertions
   assert.is(document.body.innerHTML.length > 600, true, 'body has content');
