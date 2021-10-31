d8a8a9286986ac9bdb43db6339555011a09fd498
Max Milton <max@wearegenki.com>
2021-10-31 18:12:34 +1000

    Simplify logo in header

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -3,22 +3,20 @@ import { h, S1Node } from 'stage1';
 export type HeaderComponent = S1Node & HTMLElement;
 
 type RefNodes = {
-  name: HTMLHeadingElement;
+  a: HTMLAnchorElement;
 };
 
 const view = h`
   <header class="microdoc-header dfc pa2">
-    <a href=/ class="microdoc-logo dib text">
-      <h2 #name class=mv0></h2>
-    </a>
+    <a #a href=/ class="microdoc-logo text"></a>
   </header>
 `;
 
 export function Header(): HeaderComponent {
   const root = view as HeaderComponent;
-  const { name } = view.collect<RefNodes>(root);
+  const { a } = view.collect<RefNodes>(root);
 
-  name.textContent = window.microdoc.title;
+  a.textContent = window.microdoc.title;
 
   return root;
 }
