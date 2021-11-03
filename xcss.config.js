3832c2041dd297255c66f4f90122658ffb6b5d52
Max Milton <max@wearegenki.com>
2021-11-03 14:14:19 +1000

    Fix lint errors

--- xcss.config.js
+++ xcss.config.js
@@ -1,5 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable import/no-extraneous-dependencies */
 
