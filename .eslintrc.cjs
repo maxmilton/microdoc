1408ec0f4a4fb753228810e0475db1692790b34e
Max Milton <max@happycode.ltd>
2022-08-14 14:20:31 +1000

    chore: Improve lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,7 @@
 const OFF = 0;
 const WARN = 1;
+const ERROR = 2;
 
-// TODO: Types
-// eslint-disable-next-line max-len
-// /** @type {import('eslint/lib/shared/types').ConfigData & { parserOptions: import('@typescript-eslint/types').ParserOptions }} */
 module.exports = {
   root: true,
   reportUnusedDisableDirectives: true,
@@ -22,7 +20,7 @@ module.exports = {
     'plugin:unicorn/recommended',
   ],
   rules: {
-    '@typescript-eslint/explicit-module-boundary-types': WARN,
+    '@typescript-eslint/explicit-module-boundary-types': ERROR,
     'import/prefer-default-export': OFF,
     // usually transpiled out
     'no-continue': WARN,
@@ -36,8 +34,7 @@ module.exports = {
     'unicorn/no-null': OFF,
     'unicorn/prefer-add-event-listener': OFF,
     'unicorn/prefer-dom-node-append': OFF,
-    'unicorn/prefer-module': OFF,
-    'unicorn/prefer-node-protocol': OFF,
+    'unicorn/prefer-module': WARN,
     // can't be polyfilled and browser support is still lacking
     'unicorn/prefer-optional-catch-binding': OFF,
     'unicorn/prefer-query-selector': OFF,
